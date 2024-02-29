80af2ca5833f5063e6ce16fe3decb2da5f41d5ec
stephenolamide <dada.stephenolamide@gmail.com>
2024-02-29 13:26:44 +0100

    added ticket screen

--- screens/profile/TicketScreen.js
+++ screens/profile/TicketScreen.js
@@ -11,17 +11,19 @@ import EncryptedStorage from 'react-native-encrypted-storage';
 import AppHeader from '../../components/AppHeader';
 import {
   BORDERRADIUS,
-  COLORS,
   FONTFAMILY,
   FONTSIZE,
   SPACING,
 } from '../../constants/theme';
 import LinearGradient from 'react-native-linear-gradient';
 import { ReusableIcon } from '../../constants/icons';
+import { getTheme } from '../../context/ThemeContext';
 
 
 const TicketScreen = ({navigation, route}) => {
   const [ticketData, setTicketData] = useState(route.params);
+
+  const {theme} = getTheme()
 
   useEffect(() => {
     (async () => {
@@ -54,6 +56,103 @@ const TicketScreen = ({navigation, route}) => {
       </View>
     );
   }
+
+
+
+  const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      flex: 1,
+      backgroundColor: theme.black,
+    },
+    appHeaderContainer: {
+      marginHorizontal: SPACING.space_36,
+      marginTop: SPACING.space_20 * 2,
+    },
+    ticketContainer: {
+      flex: 1,
+      justifyContent: 'center',
+    },
+    ticketBGImage: {
+      alignSelf: 'center',
+      width: 300,
+      aspectRatio: 200 / 300,
+      borderTopLeftRadius: BORDERRADIUS.radius_25,
+      borderTopRightRadius: BORDERRADIUS.radius_25,
+      overflow: 'hidden',
+      justifyContent: 'flex-end',
+    },
+    linearGradient: {
+      height: '70%',
+    },
+    linear: {
+      borderTopColor: theme.black,
+      borderTopWidth: 3,
+      width: 300,
+      alignSelf: 'center',
+      backgroundColor: theme.primaryblue,
+      borderStyle: 'dashed',
+    },
+    ticketFooter: {
+      backgroundColor: theme.primaryblue,
+      width: 300,
+      alignItems: 'center',
+      paddingBottom: SPACING.space_36,
+      alignSelf: 'center',
+      borderBottomLeftRadius: BORDERRADIUS.radius_25,
+      borderBottomRightRadius: BORDERRADIUS.radius_25,
+    },
+    ticketDateContainer: {
+      flexDirection: 'row',
+      gap: SPACING.space_36,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: SPACING.space_10,
+    },
+    ticketSeatContainer: {
+      flexDirection: 'row',
+      gap: SPACING.space_36,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: SPACING.space_10,
+    },
+    dateTitle: {
+      fontFamily: FONTFAMILY.poppins_medium,
+      fontSize: FONTSIZE.size_24,
+      color: theme.white,
+    },
+    subtitle: {
+      fontFamily: FONTFAMILY.poppins_regular,
+      fontSize: FONTSIZE.size_14,
+      color: theme.white,
+    },
+    subheading: {
+      fontFamily: FONTFAMILY.poppins_medium,
+      fontSize: FONTSIZE.size_18,
+      color: theme.white,
+    },
+    subtitleContainer: {
+      alignItems: 'center',
+    },
+    clockIcon: {
+      fontSize: FONTSIZE.size_24,
+      color: theme.white,
+      paddingBottom: SPACING.space_10,
+    },
+    barcodeImage: {
+      height: 50,
+      aspectRatio: 158 / 52,
+    },
+    blackCircle: {
+      height: 80,
+      width: 80,
+      borderRadius: 80,
+      backgroundColor: theme.black,
+    },
+  });
+
+
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
@@ -67,10 +166,12 @@ const TicketScreen = ({navigation, route}) => {
 
       <View style={styles.ticketContainer}>
         <ImageBackground
-          source={{uri: ticketData?.ticketImage}}
+        //   source={{uri: ticketData?.ticketImage}}
+
+        source={require("../../assets/images/stephen.jpg")}
           style={styles.ticketBGImage}>
           <LinearGradient
-            colors={[COLORS.OrangeRGBA0, COLORS.Orange]}
+            colors={[theme.primaryblue, theme.primary]}
             style={styles.linearGradient}>
             <View
               style={[
@@ -137,96 +238,6 @@ const TicketScreen = ({navigation, route}) => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    backgroundColor: COLORS.Black,
-  },
-  appHeaderContainer: {
-    marginHorizontal: SPACING.space_36,
-    marginTop: SPACING.space_20 * 2,
-  },
-  ticketContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  ticketBGImage: {
-    alignSelf: 'center',
-    width: 300,
-    aspectRatio: 200 / 300,
-    borderTopLeftRadius: BORDERRADIUS.radius_25,
-    borderTopRightRadius: BORDERRADIUS.radius_25,
-    overflow: 'hidden',
-    justifyContent: 'flex-end',
-  },
-  linearGradient: {
-    height: '70%',
-  },
-  linear: {
-    borderTopColor: COLORS.Black,
-    borderTopWidth: 3,
-    width: 300,
-    alignSelf: 'center',
-    backgroundColor: COLORS.Orange,
-    borderStyle: 'dashed',
-  },
-  ticketFooter: {
-    backgroundColor: COLORS.Orange,
-    width: 300,
-    alignItems: 'center',
-    paddingBottom: SPACING.space_36,
-    alignSelf: 'center',
-    borderBottomLeftRadius: BORDERRADIUS.radius_25,
-    borderBottomRightRadius: BORDERRADIUS.radius_25,
-  },
-  ticketDateContainer: {
-    flexDirection: 'row',
-    gap: SPACING.space_36,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: SPACING.space_10,
-  },
-  ticketSeatContainer: {
-    flexDirection: 'row',
-    gap: SPACING.space_36,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: SPACING.space_10,
-  },
-  dateTitle: {
-    fontFamily: FONTFAMILY.poppins_medium,
-    fontSize: FONTSIZE.size_24,
-    color: COLORS.White,
-  },
-  subtitle: {
-    fontFamily: FONTFAMILY.poppins_regular,
-    fontSize: FONTSIZE.size_14,
-    color: COLORS.White,
-  },
-  subheading: {
-    fontFamily: FONTFAMILY.poppins_medium,
-    fontSize: FONTSIZE.size_18,
-    color: COLORS.White,
-  },
-  subtitleContainer: {
-    alignItems: 'center',
-  },
-  clockIcon: {
-    fontSize: FONTSIZE.size_24,
-    color: COLORS.White,
-    paddingBottom: SPACING.space_10,
-  },
-  barcodeImage: {
-    height: 50,
-    aspectRatio: 158 / 52,
-  },
-  blackCircle: {
-    height: 80,
-    width: 80,
-    borderRadius: 80,
-    backgroundColor: COLORS.Black,
-  },
-});
+
 
 export default TicketScreen;
